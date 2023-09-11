34c2b049bac0dd02885e7fba4011dab0cb805522
Vlad17082005 <super_Vladlen2005@ukr.net>
2023-09-11 12:02:21 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/account', method=['POST', 'GET'])
+@login_required
+def account():
+    form = UpdateAccountDetailsForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.password = form.password.data
+        current_user.year_group = form.year_group.data
+        current_user.house = form.house.data
+        db.session.commit()
+    elif:
+    return
+
+
+if __name__ == '__main__':
+    app.run()
